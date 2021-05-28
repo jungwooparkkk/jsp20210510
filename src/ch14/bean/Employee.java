f452adf5759319b7f347b4a29839b8b894ec251d
Jungwooparkkk <pjw7935@naver.com>
2021-05-28 16:58:19 +0900

    jdbc

---
+++ src/ch14/bean/Employee.java
@@ -0,0 +1,27 @@
+package ch14.bean;
+
+public class Employee {
+	private String employeeIdid;
+	private String lastName;
+	private String firstName;
+	public String getEmployeeId() {
+		return employeeIdid;
+	}
+	public void setEmployeeId(String employeeIdid) {
+		this.employeeIdid = employeeIdid;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
+}
