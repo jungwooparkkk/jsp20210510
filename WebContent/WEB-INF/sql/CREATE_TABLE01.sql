b2134bbb5edb5da710615c7db2a7b55995eb8328
Jungwooparkkk <pjw7935@naver.com>
2021-06-02 21:45:18 +0900

    jdbc

---
+++ WebContent/WEB-INF/sql/CREATE_TABLE01.sql
@@ -0,0 +1,33 @@
+CREATE TABLE MyTable1 (
+	col1 INT
+);
+
+DESC MyTable1;
+
+INSERT INTO MyTable1
+(col1)
+VALUES (888);
+
+SELECT * FROM MyTable1;
+
+DELETE FROM MyTable1
+WHERE Col1 = 999;
+
+UPDATE MyTable1
+SET Col1 = 777
+WHERE Col1 = 888;
+
+CREATE TABLE MyTable2
+(
+	col1 INT,
+    Col2 INT
+);
+
+DESC MyTable2;
+
+INSERT INTO MyTable2
+(col1, Col2)
+VALUES
+(987, 123);
+SELECT * FROM MyTable2;
+
