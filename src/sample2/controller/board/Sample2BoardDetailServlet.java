bdb9144aef5826b71c069ba0f14b94db4782fbb8
Jungwooparkkk <pjw7935@naver.com>
2021-06-04 16:16:23 +0900

    jdbc

--- src/sample2/controller/board/Sample2BoardDetailServlet.java
+++ src/sample2/controller/board/Sample2BoardDetailServlet.java
@@ -1,25 +1,27 @@
-package sample2.controller.member;
+package sample2.controller.board;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import sample2.dao.MemberDao;
+import sample2.bean.Board;
+import sample2.dao.BoardDao;
 
 /**
- * Servlet implementation class Sample2CheckDupServlet
+ * Servlet implementation class Sample2BoardDetailServlet
  */
-@WebServlet("/sample2/member/checkdup")
-public class Sample2CheckDupServlet extends HttpServlet {
+@WebServlet("/sample2/board/detail")
+public class Sample2BoardDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Sample2CheckDupServlet() {
+    public Sample2BoardDetailServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,27 +30,29 @@ public class Sample2CheckDupServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String id = request.getParameter("id");
+		
+		if (id == null) {
+			String path = request.getContextPath() + "/sample2/board/list";
+			response.sendRedirect(path);
+		} else {
+			BoardDao dao = new BoardDao();
+			Board board = dao.get(Integer.parseInt(id));
+			
+			request.setAttribute("board", board);
+			
+			String path = "/WEB-INF/sample2/board/detail.jsp";
+			request.getRequestDispatcher(path).forward(request, response);
+		}
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String id = request.getParameter("id");
-		
-		// System.out.println(id);
-		
-		MemberDao dao = new MemberDao();
-		
-		response.setContentType("text/plain; charset=utf-8");
-		if (dao.existsId(id)) {
-			response.getWriter().append("not ok");
-		} else {
-			response.getWriter().append("ok");
-		}
-		
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
